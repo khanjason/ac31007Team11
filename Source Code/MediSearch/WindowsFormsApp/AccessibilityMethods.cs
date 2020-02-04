ef04e705bbcdb7fb63ff17ef015823bf9083ffaf
khanjason <jasonxdecember@gmail.com>
2020-02-04 12:48:21 +0000

    added methods, added validation test

---
+++ Source Code/MediSearch/WindowsFormsApp/AccessibilityMethods.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    class AccessibilityMethods
+
+    {
+
+        public bool nospecialcharacters(string fontsize)
+        {
+            char[] fontsizearr = fontsize.ToCharArray();
+            char[] speiChars = new char[] { '!', ',', '?', '#', '£', '$', '^', '&', '*', '(', ')' };
+            foreach (var t in fontsize)
+                if ((speiChars.Contains(t)))
+                    return false;
+
+            return true;
+
+        }
+
+        public bool containsnumbers(string fontsize)
+
+        { 
+            char[] fontsizearr = fontsize.ToCharArray();
+            char[] nums = new char[] { '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            for (int i = 0; i < fontsizearr.Length; i++)
+            {
+                if (!(nums.Contains(fontsizearr[i])))
+                    return false;
+
+            }
+            return true;
+        }
+
+
+        public void changeFontSize(string s)
+        {
+            if (!(containsnumbers(s) || !(nospecialcharacters(s))))
+               throw new SystemException("Invalid font size");
+
+            float size;
+            size = Convert.ToSingle(s, new CultureInfo("en-gb"));
+            Form f;
+            f = new map { Font = new Font(Control.DefaultFont.FontFamily.ToString(), size) };
+
+        }
+
+        public void changeFontColour(string col)
+        {
+            Form f;
+            f = new map();
+            Color colour = f.ForeColor;
+            switch (col)
+            {
+                case "Red":
+                    colour = Color.Red;
+                    break;
+                case "Blue":
+                    colour = Color.Blue;
+                    break;
+                case "Orange":
+                    colour = Color.Orange;
+                    break;
+            }
+
+            f.ForeColor = colour;
+
+        }
+
+
+    }
+}
--- Source Code/MediSearch/WindowsFormsApp/AccessibilityTesting.cs
+++ Source Code/MediSearch/WindowsFormsApp/AccessibilityTesting.cs
@@ -14,6 +14,51 @@ namespace WindowsFormsApp
 {
     public class AccessibilityTesting
     {
+        public static IEnumerable<object[]> GetFontSizes(int numTests)
+        {
+            var allData = new List<object[]>
+            {
+                new object[] { 18 },
+                new object[] { "jeff" },
+                new object[] { "32.5" },
+                new object[] {"18" },
+            };
+
+            return allData.Take(numTests);
+        }
+
+        public bool containsnumbers(string fontsize)
+
+        {
+
+            char[] fontsizearr = fontsize.ToCharArray();
+            char[] nums = new char[] { '.','1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            for (int i = 0; i < fontsizearr.Length; i++)
+            {
+                if (!(nums.Contains(fontsizearr[i])))
+                    return false;
+
+            }
+
+            return true;
+
+        }
+
+        public bool nospecialcharacters(string fontsize)
+        {
+            char[] fontsizearr = fontsize.ToCharArray();
+            char[] speiChars = new char[] { '!', ',',  '?', '#', '£', '$', '^', '&', '*', '(', ')' };
+            foreach (var t in fontsize)
+                if ((speiChars.Contains(t)))
+                    return false;
+
+            return true;
+
+        }
+
+
+
+
         [Fact]
         public void Test_getfontsize()
         {
@@ -65,6 +110,22 @@ namespace WindowsFormsApp
 
             Assert.True(r,"font colour not changed");
         }
-        
+
+        [Theory]
+        [MemberData(nameof(GetFontSizes), parameters: 3)]
+        public void validateSizeinput(string size)
+        {
+            bool r;
+            if (containsnumbers(size) && (nospecialcharacters(size)))
+                r = true;
+            else
+                r = false;
+
+            Assert.True(r,"font size entered not valid");
+
+
+        }
+
+
     }
 }

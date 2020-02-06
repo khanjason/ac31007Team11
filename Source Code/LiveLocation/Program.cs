4a9313fe89e9674205bf546064198be0e08e5a62
melvinbinu <60140649+melvinbinu@users.noreply.github.com>
2020-02-06 11:57:13 +0000

    Added Live Location feature code

---
+++ Source Code/LiveLocation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Device.Location;
+
+namespace LiveLocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetLocationProperty();
+        }
+
+        static void GetLocationProperty()
+        {
+            GeoCoordinateWatcher watcher;
+            watcher = new GeoCoordinateWatcher();
+
+            watcher.PositionChanged += (sender, e) =>
+            {
+                var coordinate = e.Position.Location;
+                Console.WriteLine("Latitude: {0}, Longtitude: {1}", coordinate.Latitude, coordinate.Longitude);
+                //One Event
+                watcher.Stop();
+            };
+
+            // Listening for location updates
+            watcher.Start();
+            Console.ReadLine();
+        }
+    }
+}
